a1334e0d2164fd7cd14b4df20b87db2e99f5ff16
aecoaker <79765956+aecoaker@users.noreply.github.com>
2022-10-30 12:31:40 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+
+Coaker, Agnes (TRADE)
+12:24 PM (5 minutes ago)
+to me
+
+from pathlib import Path
+
+ 
+
+DATA_DIR = {
+
+    'UK-AU-2021': {
+
+        'url': 'https://www.gov.uk/government/collections/free-trade-agreement-between-the-united-kingdom-of-great-britain-and-northern-ireland-and-australia'
+
+    },
+
+    'UK-NZ-2022': {
+
+        'url': 'https://www.gov.uk/government/collections/free-trade-agreement-between-the-united-kingdom-of-great-britain-and-northern-ireland-and-new-zealand'
+
+    }
+
+}
+
+ROOT_DIR = Path(__file__).parent.parent
